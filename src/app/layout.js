5115ec0b4ae809cc0473ac9e4a3d7d7963f2aa32
AdyashaDas1 <150039955+AdyashaDas1@users.noreply.github.com>
2026-05-23 13:26:44 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,10 @@
-// import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
 
-// const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
-// const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
-
-// export const metadata = {
-//   title: "Adyasha Das — Frontend Developer",
-//   description: "Creative Frontend Developer specializing in Next.js, TypeScript, and modern UI",
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-//       <body>{children}</body>
-//     </html>
-//   );
-// }
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -45,6 +29,7 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable}`}
     >
       <body>
+        <ScrollToTop />
         <Navbar />
 
         {children}
